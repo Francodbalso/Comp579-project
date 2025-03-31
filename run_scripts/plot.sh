fabce715a8448db539609d396ad0ac3fe6dca8ca
Franco Del Balso <fdelba@teach-node-05.CS.McGill.CA>
2025-03-31 16:34:47 -0400

    edited run scripts

--- run_scripts/plot.sh
+++ run_scripts/plot.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes 1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=2G
-#SBATCH --time=00:50:00
+#SBATCH --mem=10G
+#SBATCH --time=00:02:00
 #SBATCH --output=%N-%j.out
 #SBATCH --qos=comp579-0gpu-4cpu-72h
 #SBATCH --account=winter2025-comp579 
 
 module load miniconda/miniconda-winter2025
 
-python ../test.py
+python ../plot.py
